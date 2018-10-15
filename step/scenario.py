5cc7dec9130c41715d3295514f49e458aaeef377
literale <43133944+literale@users.noreply.github.com>
2018-10-15 11:31:34 +0600

    Логи

--- step/scenario.py
+++ step/scenario.py
@@ -1,20 +1,16 @@
-from behave import *
 from selenium import webdriver
+from behave import *
 import time
 from datetime import datetime
 from selenium.common.exceptions import *
 
-with open("log.txt", "a") as log:
-    log.write("{0} Сценарий выполнен.\n".format(datetime.now()))
 
+driver = webdriver.Firefox(executable_path=r"geckodriver.exe")
 
-@when('Зашли на сайт "{site_name}"')
 def step_impl(context, site_name):
     """
     :type context: behave.runner.Context
     """
-    # context.driver.get("http://google.ru")
-    context.driver = webdriver.Chrome("chromedriver.exe")
     context.driver.get(site_name)
 
 @step('Кликаем на кнопку Логина')
@@ -26,6 +22,8 @@ def step_impl(context):
 def step_impl(context):
     try:
         log_btn = driver.find_element_by_id('index_login_button')
-        log.write("{0} Мы  не залогинились!\n".format(datetime.now()))
+        with open("log.txt", "a") as log:
+            log.write("{0} Мы  не залогинились!\n".format(datetime.now()))
     except NoSuchElementException:
-        log.write("{0} Мы залогинились!\n".format(datetime.now()))
+        with open("log.txt", "a") as log:
+            log.write("{0} Мы залогинились!\n".format(datetime.now()))
