12b1da71d26dc2e9d1d8c9cfc8275ac31e8350ca
literale <43133944+literale@users.noreply.github.com>
2018-10-15 12:50:42 +0600

    удален лишний файл

--- features/steps/login.py
+++
@@ -1,9 +0,0 @@
-from behave import *
-
-@Given('зашли на сайт "{site_name}"')
-def step_impl(context, site_name):
-    """
-    :type context: behave.runner.Context
-    """
-    pass
-    # raise NotImplementedError(u'STEP: And зашли на сайт "www.google.ru"')
