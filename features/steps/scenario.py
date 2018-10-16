6ce5bb0568361e9dc1ad4186e53f570dbe5f8535
literale <43133944+literale@users.noreply.github.com>
2018-10-16 17:44:20 +0600

    ПОчинены вэйты и путь к драйверу

--- features/steps/scenario.py
+++ features/steps/scenario.py
@@ -1,21 +1,27 @@
+from Tools.scripts.win_add2path import PATH
 from selenium import webdriver
 from behave import *
 import time
 from datetime import datetime
 from selenium.common.exceptions import *
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import logging
 import os
 
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+
 logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
                     filename="log.log", level=logging.INFO)
-
 
 @when('Зашли на сайт "{site_name}"')
 def step_impl(context, site_name):
     """
     :type context: behave.runner.Context
     """
-    context.driver = webdriver.Firefox(executable_path=r"D:\\gecodrive\\geckodriver.exe")
+    #context.driver = webdriver.Firefox(executable_path=r"D:\\gecodrive\\geckodriver.exe")
+    context.driver = webdriver.Firefox(executable_path=r"geckodriver.exe")
     context.driver.get(site_name)
     context.driver.delete_all_cookies()
 
@@ -47,7 +53,9 @@ def step_impl(context, password):
 
 @then('Приверяем страницу')
 def step_impl(context):
-    time.sleep(5)
+    wait_for_element = WebDriverWait(context.driver, 10).until(
+        EC.visibility_of_element_located((By.ID, "login_reg_button"))
+    )
     if context.driver.current_url == "https://vk.com/":
         logging.info('Мы Не сменили страницу!')
         #print("Мы Не сменили страницу!")
@@ -72,7 +80,9 @@ def step_impl(context):
 
 @step('Опять проверяем страницу')
 def step_impl(context):
-    time.sleep(5)
+    wait_for_element = WebDriverWait(context.driver, 10).until(
+        EC.visibility_of_element_located((By.ID, "ij_submit"))
+    )
     if context.driver.current_url == "https://vk.com/":
         logging.info('Мы на первой странице!')
        # print("Мы на первой странице!")
